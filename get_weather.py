121a834272acdff281a69ab56bee97f06a383191
Luke Rawilns <luke@lukerawlins.com>
2024-04-15 14:33:13 -0400

    version 1 complete

--- get_weather.py
+++ get_weather.py
@@ -9,7 +9,7 @@ class location():
     def __init__(self, city, state):
         self.city = city.title()
         self.state = state.title()
-        print(self.city + ', ' +  self.state)
+#        print(self.city + ', ' +  self.state)
         self.geo_url='https://geocoding-api.open-meteo.com/v1/search'
         self.params = {
             "name": self.city,
@@ -21,7 +21,6 @@ class location():
             raise SystemExit(e)
 
         for i in r.json()['results']:
-            #print(json.dumps(r.json(),indent=4))
             if i['admin1'] == self.state:
                 self.lat = i['latitude']
                 self.long = i['longitude']
@@ -30,6 +29,19 @@ class location():
                 break
             else:
                 continue
+    def get_city(self):
+        try:
+            self.city
+        except NameError as e:
+            raise SystemExit(e)
+        return self.city
+
+    def get_state(self):
+        try:
+            self.state
+        except NameError as e:
+            raise SystemExit(e)
+        return self.state
 
     def get_lat(self):
         try:
@@ -68,7 +80,7 @@ class forecast():
             "latitude": lat,
             "longitude": long,
             "current": "temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,cloud_cover",
-            #rain,showers,snowfall,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10"
+            #wind_speed_10m,wind_direction_10m,wind_gusts_10"
             "temperature_unit": "fahrenheit",
             "wind_speed_unit": "mph",
             "precipitation_unit": "inch",
@@ -81,10 +93,24 @@ class forecast():
         except requests.exceptions.RequestException as e:
             raise SystemExit(e)
 
-    def get_forecast(self):
-        print(self.api_lic)
-        print(self.api_lnk)
-        return self.r.json()
+    def get_time(self):
+        #print(self.api_lic)
+        #print(self.api_lnk)
+        return self.r.json()['current']['time']
+    def get_temp(self):
+        #print(self.api_lic)
+        #print(self.api_lnk)
+        temp = self.r.json()['current']['temperature_2m']
+        unit = self.r.json()['current_units']['apparent_temperature']
+        return (temp,unit)
+    def get_feels(self):
+        #print(self.api_lic)
+        #print(self.api_lnk)
+        return self.r.json()['current']['apparent_temperature']
+    def get_humid(self):
+        #print(self.api_lic)
+        #print(self.api_lnk)
+        return self.r.json()['current']['relative_humidity_2m']
 
 parser = argparse.ArgumentParser(
                     formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -109,8 +135,14 @@ lat = l.get_lat()
 long = l.get_long()
 tz = l.get_tz()
 # Finally pass in latitude and longitude to get forecast
-f = forecast(lat, long, tz).get_forecast()
-## Todo: parse this json and print something nicer.
-print(json.dumps(f,indent=4))
+#f = forecast(lat, long, tz).get_forecast()
+f = forecast(lat, long, tz)
 
+print("Forcast for " + l.get_city() +", " + l.get_state())
+print("Temperature => " + ''.join(str(f.get_temp()[0]))+''.join(str(f.get_temp()[1])))
+print("Feels Like  => " + ''.join(str(f.get_feels()))+''.join(str(f.get_temp()[1])))
+print("Humidity    => " + str(f.get_humid()) + "%")
+print("----------------------------------")
+print("Weather data by Open-Meteo.com")
+print("https://open-meteo.com/")
 
