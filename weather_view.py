104742323f1f402f6da59f09fd7ec0189d6635a7
Gregory F Martin <greg@gregfmartin.org>
2024-04-17 15:36:41 -0400

    Effing linter

--- weather_view.py
+++ weather_view.py
@@ -1,11 +1,21 @@
-# pylint: disable=line-too-long, superfluous-parens
+# pylint: disable=line-too-long, superfluous-parens, too-few-public-methods
 
 # GFM :: WEATHER-VIEW
 #
 # CONSOLIDATES THE WEATHER INFORMATION INTO A GRAPHICAL DISPLAY
+
+"""
+WeatherView Module
+
+Defines a view for weather information that can be inserted into a Table Cell.
+"""
+
 import ansi
 
 class WeatherView:
+    """
+    Defines a WeatherView object.
+    """
     def __init__(self, **kwargs) -> None:
         self.draw_offset      = kwargs['draw_offset'] if 'draw_offset' in kwargs else ansi.ATCoordinatesNone()
         self.weather_alert    = kwargs['weather_alert'] if 'weather_alert' in kwargs else None
@@ -16,6 +26,9 @@ class WeatherView:
         self.precip_display   = kwargs['precip_display'] if 'precip_display' in kwargs else ansi.ATStringComposite()
 
     def draw(self) -> str:
+        """
+        Populates the data from this view into a string with ANSI SGRs and returns that to the caller.
+        """
         a = ''
 
         a += f"{ansi.ATCoordinates(row = (self.draw_offset.row + 1), col = (self.draw_offset.col + 1)).to_ansi_str()}{self.weather_alert.to_ansi_str()}"
@@ -23,4 +36,6 @@ class WeatherView:
         a += f"{ansi.ATCoordinates(row = (self.draw_offset.row + 4), col = (self.draw_offset.col + 1)).to_ansi_str()}{self.weather_art}"
         a += f"{ansi.ATCoordinates(row = (self.draw_offset.row + 10), col = (self.draw_offset.col + 1)).to_ansi_str()}{self.temp_display.to_ansi_str()}"
         a += f"{ansi.ATCoordinates(row = (self.draw_offset.row + 10), col = (self.draw_offset.col + 7)).to_ansi_str()}{self.humidity_display.to_ansi_str()}"
-        
+        a += f"{ansi.ATCoordinates(row = (self.draw_offset.row + 11), col = (self.draw_offset.col + 1)).to_ansi_str()}{self.precip_display.to_ansi_str()}"
+
+        return a
