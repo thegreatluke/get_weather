b614b5abf095b75854fe6c972163a2c1a97f7c47
Gregory F Martin <greg@gregfmartin.org>
2024-04-17 16:10:14 -0400

    Stashing this.

--- weather_view.py
+++ weather_view.py
@@ -1,4 +1,4 @@
-# pylint: disable=line-too-long, superfluous-parens, too-few-public-methods
+# pylint: disable=line-too-long, superfluous-parens, too-few-public-methods, too-many-instance-attributes
 
 # GFM :: WEATHER-VIEW
 #
@@ -10,6 +10,7 @@ WeatherView Module
 Defines a view for weather information that can be inserted into a Table Cell.
 """
 
+import sys
 import ansi
 
 class WeatherView:
@@ -17,6 +18,7 @@ class WeatherView:
     Defines a WeatherView object.
     """
     def __init__(self, **kwargs) -> None:
+        self.weather_data     = kwargs['weather_data'] if 'weather_data' in kwargs else None
         self.draw_offset      = kwargs['draw_offset'] if 'draw_offset' in kwargs else ansi.ATCoordinatesNone()
         self.weather_alert    = kwargs['weather_alert'] if 'weather_alert' in kwargs else None
         self.day_display      = kwargs['day_display'] if 'day_display' in kwargs else ansi.ATStringComposite()
@@ -24,6 +26,17 @@ class WeatherView:
         self.temp_display     = kwargs['temp_display'] if 'temp_display' in kwargs else ansi.ATStringComposite()
         self.humidity_display = kwargs['humidity_display'] if 'humidity_display' in kwargs else ansi.ATStringComposite()
         self.precip_display   = kwargs['precip_display'] if 'precip_display' in kwargs else ansi.ATStringComposite()
+
+    def interpolate_weather_data(self) -> None:
+        if self.weather_data is not None:
+            # WE CAN DO SOMETHING WITH THE DATA
+            # ASSUME THAT THIS IS THE JSON-CONVERTED OBJECT
+            # CHECK TO SEE IF THE DRAW OFFSET HAS BEEN SET
+            # OTHERWISE, WE CAN'T DO MUCH HERE (THROW -99)
+            if self.draw_offset is not None:
+                pass
+            else:
+                sys.exit(-99)
 
     def draw(self) -> str:
         """
